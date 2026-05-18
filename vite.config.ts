a0622e564ff7b1a09b8fb4e8379bdfbd599df271
hoangkenlee <hoangkenlee@gmail.com>
2026-05-18 16:43:18 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,14 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
+    optimizeDeps: {
+      include: ['xlsx']
+    },
+    build: {
+      commonjsOptions: {
+        include: [/xlsx/, /node_modules/],
+      },
+    },
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
